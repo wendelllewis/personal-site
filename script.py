a4b044fa887a51e712e61aebe802228c2d52b355
Wendell <wendell1126@icloud.com>
2024-10-20 19:31:36 -0600

    Updated script.py to preserve input format

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 def input_parse(file_path):
     headers = []
     paragraphs = []
-
+    images = []
 
     with open(file_path, "r") as file:
         for line_number, line in enumerate(file, start=1):
@@ -10,22 +10,32 @@ def input_parse(file_path):
                 headers.append(stripped_line)
             elif stripped_line.startswith('#'):
                 paragraphs.append(stripped_line)
+            elif stripped_line.startswith('@'):
+                images.append(stripped_line)
     headers_final = [item[2:] for item in headers]
     paragraphs_final = [item[1:] for item in paragraphs]
+    images_final = [item[1:] for item in images]
+
     return headers_final, paragraphs_final
 
-def writetofile(file_path, headers, index):
+def writetofile(file_path, headers, paragraphs):
     with open(file_path, "r") as file:
         file_content = file.readlines()
-        file_content.insert(index+10, f"<h1>{headers[index]}</h1>\n")
-
+    for count, header in enumerate(headers):
+        file_content.insert(count * 2 + 9, f"<h1>{header}</h1>\n")
+        if count < len(paragraphs):
+            file_content.insert(count * 2 + 10, f"<p>{paragraphs[count]}</p>\n")
     with open("./articles-html/10.14.24.html", "w") as file:
         file.writelines(file_content)
 
-headers, paragraphs = input_parse("./articles-text/test-article.txt")
+input_name = input('input name?')
+output_name = input('output name?')
 
-for count, item in enumerate(headers):
-    writetofile("./articles-html/10.14.24.html", headers, count)
+input_path = f"./articles-text/{input_name}.txt"
+output_path = f'./articles-html/{output_name}.html'
+
+headers, paragraphs = input_parse(input_path)
+writetofile(output_path, headers, paragraphs)
 
 
 
