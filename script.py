989d56dd918d44dd7c19f11e511e2f35c82f9cf2
Wendell <wendell1126@icloud.com>
2024-10-25 20:50:37 -0600

    Streamlined python script

--- script.py
+++ script.py
@@ -1,33 +1,24 @@
 def input_parse(file_path):
-    headers = []
-    paragraphs = []
-    images = []
+    lines = []
 
     with open(file_path, "r") as file:
         for line_number, line in enumerate(file, start=1):
             stripped_line = line.strip()
-            if stripped_line.startswith('##'):
-                headers.append(stripped_line)
-            elif stripped_line.startswith('#'):
-                paragraphs.append(stripped_line)
-            elif stripped_line.startswith('@'):
-                images.append(stripped_line)
-    headers_final = [item[2:] for item in headers]
-    paragraphs_final = [item[1:] for item in paragraphs]
-    images_final = [item[1:] for item in images]
-
-    return headers_final, paragraphs_final, images_final
-
-def writetofile(file_path, headers, paragraphs, images):
+            lines.append(stripped_line)
+        for i, element in enumerate(lines):
+            if element.startswith('##'):
+                lines[i] = '<h1>' + element[2:] + '</h1>'
+            elif element.startswith('#'):
+                lines[i] = '<p>' + element[1:] + '</p>'
+            elif element.startswith('@'):
+                lines[i] = '<img src="' + element[1:] + '">'
+    return lines
+def writetofile(file_path, code):
     with open(file_path, "r") as file:
         file_content = file.readlines()
-    for count, header in enumerate(headers):
-        file_content.insert(count * 2 + 9, f"<h1>{header}</h1>\n")
-        if count < len(paragraphs):
-            file_content.insert(count * 2 + 10, f"<p>{paragraphs[count]}</p>\n")
-    for count, image in enumerate(images):
-        file_content.insert(count * 2 + 11, f'<img src={images[count]} alt=>\n')
-        print('remember to add alt text to images!')
+    for count, line in enumerate(code):
+        file_content.insert(count + 10, line + '\n')
+        file_content.insert(count + 11, '\n')
     with open("./articles-html/10.14.24.html", "w") as file:
         file.writelines(file_content)
 
@@ -37,8 +28,25 @@ output_name = input('output name?')
 input_path = f"./articles-text/{input_name}.txt"
 output_path = f'./articles-html/{output_name}.html'
 
-headers, paragraphs, images = input_parse(input_path)
-writetofile(output_path, headers, paragraphs, images)
+article_text = input_parse(input_path)
+writetofile(output_path, article_text)
+
+
+
+
+
+
+
+
+
+# input_name = input('input name?')
+# output_name = input('output name?')
+
+# input_path = f"./articles-text/{input_name}.txt"
+# output_path = f'./articles-html/{output_name}.html'
+
+# headers, paragraphs, images = input_parse(input_path)
+# writetofile(output_path, headers, paragraphs, images)
 
 
 
